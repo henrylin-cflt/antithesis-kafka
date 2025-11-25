a72a276688e9098e436b2d8c6212d562dfe29005
Connor McKee <connor.mckee@antithesis.com>
2025-11-24 21:22:55 -0500

    less producers, less topics, less messages per batch -> more opps for collision

--- workload/src/kafka/test_producer.rs
+++ workload/src/kafka/test_producer.rs
@@ -54,7 +54,7 @@ impl TestProducer {
         // Initialize transactions only if enabled
         if config.enable_transactions {
             inner_producer
-                .init_transactions(Timeout::After(Duration::from_secs(5)))
+                .init_transactions(Timeout::After(Duration::from_millis(3500)))
                 .context("failed to initialize transactions")?;
         }
         
@@ -190,7 +190,7 @@ impl TestProducer {
                 };
                 match self
                     .inner_producer
-                    .send(record, Timeout::After(Duration::from_secs(5)))
+                    .send(record, Timeout::After(Duration::from_millis(3500)))
                     .await
                 {
                     Ok((partition, offset)) => {
@@ -221,7 +221,7 @@ impl TestProducer {
         let should_abort = rng::u64_in(1, 100) <= 30;
         
         if should_abort {
-            match self.inner_producer.abort_transaction(Timeout::After(Duration::from_secs(1))) {
+            match self.inner_producer.abort_transaction(Timeout::After(Duration::from_millis(500))) {
                 Ok(_) => {
                     warn!(
                         timestamp = chrono::Utc::now()
@@ -248,7 +248,7 @@ impl TestProducer {
                 }
             }
         } else {
-            match self.inner_producer.commit_transaction(Timeout::After(Duration::from_secs(1))) {
+            match self.inner_producer.commit_transaction(Timeout::After(Duration::from_millis(500))) {
                 Ok(_) => {
                     // Now that transaction is committed, log all messages as successfully written
                     for (partition, offset, payload, msg_key) in pending_messages {
@@ -325,7 +325,7 @@ impl TestProducer {
                 };
                 match self
                     .inner_producer
-                    .send(record, Timeout::After(Duration::from_secs(5)))
+                    .send(record, Timeout::After(Duration::from_millis(3500)))
                     .await
                 {
                     Ok((partition, offset)) => {
